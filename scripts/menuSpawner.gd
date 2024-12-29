a773f3f2d43851a6456211a9f8b041fcac3c2ed1
terumafu <119095669+terumafu@users.noreply.github.com>
2024-12-28 21:58:43 -0800

    :(

--- scripts/menuSpawner.gd
+++ scripts/menuSpawner.gd
@@ -1,1 +1,13 @@
 extends Node
+var debris = preload("res://scenes/space_debris.tscn");
+
+@export var debrisHolder : Node2D;
+@export var spawnTimer : Timer;
+
+func spawns_debris(location, type, size, speed):
+	var tempDebris = debris.instantiate();
+	tempDebris.global_position = location;
+	tempDebris.set_sprite(type);
+	tempDebris.set_size(size);
+	tempDebris.set_speed(speed);
+	debrisHolder.add_child(tempDebris);
